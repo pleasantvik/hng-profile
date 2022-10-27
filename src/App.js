e669cb74f99f3e4e96c9e39fc19f914896562b8c
pleasantvik <adedayo.ayoola5683@gmail.com>
2022-10-27 17:07:54 +0100

    initial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import Profile from "./Profile";
+import Product from "./Product";
+const items = [
+  {
+    href: "https://training.zuri.team/",
+    id: "btn_zuri",
+    content: "Zuri Team",
+    desc: "",
+  },
+  {
+    href: "https://books.zuri.team/",
+    id: "books",
+    content: "Zuri books",
+    desc: "Looking from books about coding and design?- Visit Zuri Books.",
+  },
+  {
+    href: "https://books.zuri.team/python-for-beginners?ref_id=Ayomide-Ajala",
+    id: "book_python",
+    content: "Python Books",
+  },
+  {
+    href: "https://background.zuri.team",
+    id: "pitch",
+    content: "Run a quick background check to know who you are working with.",
+  },
+  {
+    href: "https://books.zuri.team/design-rules",
+    id: "book_design",
+    content: "Design Books",
+  },
+];
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Profile />
+      {items.map(({ href, id, content, desc }) => {
+        return <Product href={href} id={id} content={content} desc={desc} />;
+      })}
     </div>
   );
 }
